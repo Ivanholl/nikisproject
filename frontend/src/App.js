a332760df090e73b11fb2228584de85ea3de239e
kikiriki89 <denislava.koeva@gmail.com>
2020-06-08 13:42:53 +0300

    intalled bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import './css/App.css';
+import { Nav } from 'react-bootstrap';
 
 function App() {
     return (<div className="App">
         <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo"/>
-            <p>
-                Edit
-                <code>src/App.js</code>
-                and save to reload.
-            </p>
-            <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                Learn React
-            </a>
+            <Nav
+                activeKey="/home"
+                onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
+            >
+                <Nav.Item>
+                    <Nav.Link href="/home">Active</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey="link-1">Link</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey="link-2">Link</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey="disabled" disabled>
+                        Disabled
+    </Nav.Link>
+                </Nav.Item>
+            </Nav>
         </header>
     </div>);
 }
