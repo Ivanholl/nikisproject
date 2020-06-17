e9eaa798339e06201d5b05908d997765efc3f07d
kikiriki89 <denislava.koeva@gmail.com>
2020-06-18 00:32:21 +0300

    full page scroll effect added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import BackToTop from "react-back-to-top-button";
-
 import BackToTopBtn from './images/back-to-top-btn.png'
+import { FullPage, Slide } from 'react-full-page';
+
 import './css/App.css';
 import './css/animations.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -16,11 +17,23 @@ import Navbar from './components/Navbar.js';
 function App() {
     return (<div className="App">
         <Navbar />
-        <FirstSlide />
-        <SecondSlide />
-        <ThirdSlide />
-        <FourthSlide />
-        <FifthSlide />
+        <FullPage>
+            <Slide>
+                <FirstSlide />
+            </Slide>
+            <Slide>
+                <SecondSlide />
+            </Slide>
+            <Slide>
+                <ThirdSlide />
+            </Slide>
+            <Slide>
+                <FourthSlide />
+            </Slide>
+            <Slide>
+                <FifthSlide />
+             </Slide>
+        </FullPage>
         <BackToTop
             showOnScrollUp
             showAt={100}
