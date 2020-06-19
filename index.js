b064d3d6072fcafbd2c22d0292563126a47c7b88
Ivanholl <ivan.jelev@abv.bg>
2020-06-19 15:28:00 +0300

    more fixes

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+
 app.use(express.static(path.join(__dirname, '/frontend/build')));
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '/frontend/build', 'index.html'));
+    console.log('hit /')
+    res.sendFile(path.join(__dirname, '/frontend/build', 'index.html'));
 });
-app.listen(9000);
+app.listen(process.env.PORT || 9000);
+
+console.log('app started on port ' + process.env.PORT)
